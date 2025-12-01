d3afca4df743f3e46316eef1517ea084cb7e42f6
rohithujare <rohithujare742@gmail.com>
2025-12-01 10:50:19 +0530

    Add project files.

---
+++ ELibrarySystem/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ELibrarySystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        //public DbSet<UserMaster> UserMasters { get; set; }
+
+
+
+
+
+    }
+}
---
+++ ELibrarySystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ELibrarySystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
