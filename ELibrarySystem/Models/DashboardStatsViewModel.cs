92d259faa24989058ee8ffe39a4f4fc3d89ac5bb
Vinayak Patil <vinayakpatil1147@gmail.com>
2025-12-02 16:15:56 +0530

    Dashboard changes

---
+++ ELibrarySystem/Models/DashboardStatsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ELibrarySystem.Models
+{
+    public class DashboardStatsViewModel
+    {
+        public int TotalBooks { get; set; }
+        public int TotalBookCategories { get; set; }
+        public int TotalStudents { get; set; }
+        public int TotalSchools { get; set; }
+        public int TotalTeachers { get; set; }
+        public int MonthlyReads { get; set; }
+
+
+    }
+}
